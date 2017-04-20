ccb416dadec25677cf117cdebf009be1cf098da4
Takahiro <tsolvalouy@gmail.com>
2017-04-20 15:22:35 +0000

    (モックアップ用)レイアウトの構築

---
+++ app/controllers/mockups_controller.rb
@@ -0,0 +1,10 @@
+class MockupsController < ApplicationController
+  def pupil
+  end
+
+  def logs
+  end
+
+  def lesson
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get '/pupil', to: 'mockups#pupil'
+  get '/logs', to: 'mockups#logs'
+  get '/lesson', to: 'mockups#lesson'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
