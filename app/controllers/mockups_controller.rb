79305bd0953adda05f84829e3c512054aeb778af
Takahiro <tsolvalouy@gmail.com>
2017-04-23 05:10:26 +0000

    ビューのルーティングを設定、 進捗画面の編集(演目画面、稽古記録画面へのリンク追加)

--- app/controllers/mockups_controller.rb
+++ app/controllers/mockups_controller.rb
@@ -1,10 +1,15 @@
 class MockupsController < ApplicationController
   def pupil
+     @user = User.where(role: "pupil").first
+     @logs = @user.lesson_logs.group(:lesson_id)
   end
 
   def logs
+     @user = User.where(role: "pupil").first
+     @logs = @user.lesson_logs.where(lesson_id: params[:id])
   end
 
   def lesson
+     @lesson = Lesson.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get '/pupil', to: 'mockups#pupil'
-  get '/logs', to: 'mockups#logs'
-  get '/lesson', to: 'mockups#lesson'
+  get '/pupil', to: 'mockups#pupil', as: 'pupil'
+  get '/logs/:id', to: 'mockups#logs', as: 'logs'
+  get '/lesson/:id', to: 'mockups#lesson', as: 'lessons'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
