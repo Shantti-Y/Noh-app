c7f0eb5622c47603596b6249a7daa599cff9bc0a
Takahiro <tsolvalouy@gmail.com>
2017-04-25 14:48:13 +0000

    Lessonモデル編集

--- test/models/lesson_test.rb
+++ test/models/lesson_test.rb
@@ -5,7 +5,8 @@ class LessonTest < ActiveSupport::TestCase
   def setup
      @lesson = Lesson.new(
                            title:            "演目：　例えば",
-                           description:      "能楽の色々な説明がここに入ります。"
+                           description:      "能楽の色々な説明がここに入ります。",
+                           genre:            "二番目"
                          )
   end
 
@@ -15,6 +16,11 @@ class LessonTest < ActiveSupport::TestCase
 
   test "title should be present" do
      @lesson.title = ""
+     assert_not @lesson.valid?
+  end
+
+  test "genre should be present" do
+     @lesson.genre = ""
      assert_not @lesson.valid?
   end
 end
