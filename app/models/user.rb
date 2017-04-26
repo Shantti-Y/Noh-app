04e1d3bb250ff9c81e41ac8dc87b5ab83135a411
Takahiro <tsolvalouy@gmail.com>
2017-04-26 11:55:18 +0000

    master_id用バリデーション=>User 訂正

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,7 @@ class User < ApplicationRecord
                            allow_nil: true,
                            length: { minimum: 4 }
    has_secure_password
-   validate  :is_pupil?    # master_id用のバリデーション
+   validate  :pupil_belongs_to_master    # master_id用のバリデーション
    enum role: { master: 1, pupil: 2 }
    validates :role,        inclusion: { in: VALID_ROLE_KEYS }
    validates :style,       presence: true
@@ -28,17 +28,26 @@ class User < ApplicationRecord
       BCrypt::Password.create(string, cost: cost)
    end
 
+   def is_pupil?
+      if self.pupil?
+         return true
+      elsif self.master?
+         return false
+      end
+   end
+
 
    # TODO アソシエーションの要領で{ belongs_to: User.master, has_many: User.pupils }がしたい
    #      ex. first_pupil.master.name や first_master.pupils.countなど
 
    private
+
       # 'role'が１(master)の場合、id入力を受け付けない
-      def is_pupil?
-         if self.master? && self.master_id != nil
-            errors.add(:master_id, "can not be added because of its roke 'master'.")
-         elsif self.pupil? && !(User.find_by(id: self.master_id))
+      def pupil_belongs_to_master
+         if self.is_pupil? && !(User.find_by(id: self.master_id))
             errors.add(:master_id, "could not find the relative master.")
+         elsif !self.is_pupil? && self.master_id != nil
+            errors.add(:master_id, "can not be added because of its role 'master'.")
          end
       end
 end
